55dfc1e621cc0cca67adc4143dcb3fde932f7f40
eriquebomfim <erique.bomfim@gmail.com>
2018-07-06 12:02:48 -0300

    Add files via upload
    Including help information

--- pdfme.sh
+++ pdfme.sh
@@ -5,6 +5,19 @@
 # a give Mb size
 # @author Erique Bomfim <erique.bomfim@gmail.com>
 # @created July-2018
+#
+# pdfme.sh [fileName] [-m:-b:-mb] [firstPage] [finalPage:-a] [sizePerFile]
+# 
+# Parameters:
+# fileName -  fullpath to the file to be splitten
+# mode     -  tells script what to do
+#        [-m] merge only
+#        [-b] break file in pieces only 
+#        [-bm]break and merge 
+# firstPage (integer) - indicate page to start process
+# finalPage (mixed)   - [number] : indicate final page to process
+#		      - [-a]     : All
+# sizePerFile (integer) - Define maximum file size in megabytes			
 # ----------------------------------------------------------
 
 
