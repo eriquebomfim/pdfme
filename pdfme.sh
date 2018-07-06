ecaf7bb8d818095a00c5451a699ac474c807c713
eriquebomfim <erique.bomfim@gmail.com>
2018-07-06 11:03:13 -0300

    Add files via upload
    this script written in bash helps to split large PDF files into small pieces based on a given MB or 1 MB as default

---
+++ pdfme.sh
@@ -0,0 +1,149 @@
+# -----------------------------------------------------------
+# Pdfme
+# this is a script written in bash in order to
+# split large pdf files in small pieces regarding
+# a give Mb size
+# @author Erique Bomfim <erique.bomfim@gmail.com>
+# @created July-2018
+# ----------------------------------------------------------
+
+
+bufferSize=0
+lastPosition=0
+totalPages=0
+sizePerFile=1 #in Mb
+
+declare -a mergins=()
+declare -a merginsID=()
+merginsLength=0
+filesToMerge=""
+
+
+merge(){
+
+s=0
+
+for i in `seq 1 $merginLength`
+do
+
+: $(( s=$i-1 ))
+
+echo "mergin...${merginsID[$s]}.......$i/$merginLength"
+
+gs -sDEVICE=pdfwrite \
+	-sPAPERSIZE=a4\
+       	-dFIXEDMEDIA \
+	-dPDFFitPage \
+	-dAutoRotatePages=/None\
+	-dNOPAUSE \
+	-dBATCH \
+	-dSAFER \
+ 	-sOutputFile=out$i.pdf \
+	${mergins[$s]} > nul
+done
+
+}
+
+pdfbrk(){
+
+totalPages=$4
+fileName=$1
+do_merge=0
+do_break=0
+
+case $2 in
+	-b) do_break=1
+	;;
+	-m) do_merge=1
+	;;	
+	-bm|-mb) do_merge=1; do_break=1
+	;;	
+esac
+
+case $4 in 
+-a) totalPages=$(pdfinfo $fileName | awk 'NR==9 {print $2}')
+;;
+*) totalPages=$4
+;;
+esac
+
+if [ "$do_break" = 1 ]; then
+echo "breaking pages first..."
+for i in `seq $3 $totalPages`
+do
+: $(gs -sDEVICE=pdfwrite \
+	-sPAPERSIZE=a4\
+       	-dFIXEDMEDIA \
+	-dPDFFitPage \
+	-dAutoRotatePages=/None\
+	-dNOPAUSE \
+	-dBATCH \
+	-dSAFER \
+	-dFirstPage=$i \
+	-dLastPage=$i \
+	-sOutputFile=tmp/sample_$i.pdf $1 > nul)
+clear
+echo "Processando $i de $totalPages."
+done
+fi
+
+if [ "$do_merge" = 1 ]; then
+pdfset $3 $totalPages
+fi
+
+
+}
+
+#-----------------------------------------------------
+# Read files sizes and calculate for merging
+#-----------------------------------------------------
+
+pdfset(){
+
+: $((sizePerFile = $sizePerFile * 1024 * 1024))
+
+for i in `seq $1 $2`
+do
+	
+#read filesize
+
+fsize=$( stat -c%s "tmp/sample_$i.pdf" )
+
+if [ $(( $bufferSize + $fsize )) -gt $sizePerFile ];then
+: $(( p=$i-1 ))
+
+  mergins+=("$filesToMerge")
+  merginsID+=("$lastPosition-$p")
+  lastPosition=$i
+
+: $(( merginLength+=1 ))
+
+  if [ "$lastPosition" -lt "$totalPages" ]; then  	
+      bufferSize=0
+      filesToMerge=""
+  fi
+
+fi
+
+: $(( bufferSize+=$fsize ))	
+filesToMerge="$filesToMerge tmp/sample_$i.pdf"  
+
+if [ "$i" = "$totalPages" ]; then	
+   mergins+=("$filesToMerge")
+   merginsID+=("$lastPosition-$totalPages")
+:   $(( merginLength+=1))
+   merge
+fi
+
+done
+}
+#-----------------------------------------------------
+# [filename] [params] [startPage] [endPage]
+#-----------------------------------------------------
+
+case $5 in
+*) sizePerFile=$5
+;;
+esac
+
+pdfbrk $1 $2 $3 $4
